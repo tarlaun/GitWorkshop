e5ceba6a716425b5755dd446ea21480b5d1c98b4
Tarlan <babalgoogool@gmail.com>
2019-03-07 09:49:40 +0330

    Changed code

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        int x=15;
+        int y=9;
+        System.out.println(x+y);
     }
 }
